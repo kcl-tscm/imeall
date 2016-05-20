7fec6e99ff3757ca7294ac884b54519684cf0b85
Montmorency <lamberh@tcd.ie>
2016-05-20 11:55:03 +0100

    added a fix_json parser

---
+++ imeall/db_scripts/fix_json.py
@@ -0,0 +1,8 @@
+import os
+import sys
+from imeall.models import GBMaintenance 
+
+gbm = GBMaintenance()
+for thing in os.listdir('./'):
+  if os.path.isdir(thing) and thing[:len(sys.argv[1])] == sys.argv[1]:
+    gbm.fix_json(thing)
