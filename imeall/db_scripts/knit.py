4ab003a7a45da4dd92f14167e01687883d21d4a6
Montmorency <lamberh@tcd.ie>
2016-07-25 16:16:47 +0100

    index corrected in calc_nye and knit.py

--- imeall/db_scripts/knit.py
+++ imeall/db_scripts/knit.py
@@ -1,12 +1,13 @@
 import os
 import sys
 import glob
-from quippy import AtomsReader, AtomsWriter
+from quippy import AtomsReader, AtomsWriter, set_fortran_indexing
 
 
 #Read in a collection of traj output files
 #and dump them into a single traj file.
-
+#list of properties that can be deleted
+del_prop_lit = ['hybrid_vec', 'hybrid_1', 'weight_region1', 'weight_region1_1']
 traj_files = sorted(glob.glob('[0-9]*.traj.xyz'))
 knit_traj = AtomsWriter('full_traj.xyz') 
 
@@ -17,6 +18,9 @@ for traj_file in traj_files:
     mom = [3.0 for x in range(len(at))]
     at.set_initial_magnetic_moments(mom)
     at.add_property('mhm1', at.properties['modified_hybrid_mark_1'])
+    at.add_property('edgex', 0.0, overwrite=True)
+    at.add_property('edgey', 0.0, overwrite=True)
+    at.add_property('screw', 0.0, overwrite=True)
     for i, mark in enumerate(at.properties['modified_hybrid_mark_1']):
       if mark> 0:
         print i, mark
