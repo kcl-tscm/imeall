6e93bf29bd17198b95bcb573920a0cdcd951a2f3
Montmorency <lamberh@tcd.ie>
2016-08-15 12:32:37 +0100

    conv_alloy can now parser the Carter EAM potential.

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. Imeall documentation master file, created by
+   sphinx-quickstart on Mon Aug  8 12:35:13 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Imeall's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- imeall/potentials/conv_allo_xml.py
+++ imeall/potentials/conv_allo_xml.py
@@ -15,7 +15,7 @@ class EAMXML(object):
     self.rcut        = rcut
     self.N           = 26
     self.ds          = ds
-    self.drho        = 0.029
+    self.drho        = 0.03
 
   def plot_xml(self, xml_file_name):
     with open(xml_file_name, 'r') as f:
@@ -26,19 +26,16 @@ class EAMXML(object):
   def alloy_to_xml(self, n_spec=1, F=[], V=[], rho=[]):
   #   Converts EAM potential to quippy friendly xml. n_spec number of species.
   #    F embedding terms for each atom, rho, density terms, V pair-potentials.
-    at_types = [26,1]
-    at_pairs = [[26,26],[1,26],[1,1]]
+    at_types = [26,1,99,100]
 #Write initial Densities and Embedding Terms for each species.
     i_spec = 0
     dat_F   = len([i for i,f in enumerate(F[:,i_spec]) if i%self.ds==0])
     dat_rho = len([i for i,f in enumerate(V[:,i_spec]) if i%self.ds==0])
     dat_V   = len([i for i,f in enumerate(rho[:,i_spec]) if i%self.ds==0])
     qeam_file = open('quip.xml','w')
-
 #Write EMBEDDING and DENSITY FUNCTIONS:
     print >> qeam_file, "<EAM_ErcolAd_params n_types=\"{3}\" n_spline_V=\"{2}\" n_spline_rho=\"{0}\" n_spline_F=\"{1}\">".format(dat_rho, dat_F, dat_V, n_spec)
     for i_spec in range(n_spec):
-      print i_spec
       nsteps  = 10000
       r       = [float(i)*self.rcut/float(nsteps) for i in range(nsteps)]
       fr      = [self.drho*float(i) for i in range(0, nsteps)]
@@ -58,38 +55,43 @@ class EAMXML(object):
       print >> qeam_file, "</per_type_data>"
 
 #Now write PAIR POTENTIALS: 
-    for i_pair in range(n_pairs):
-      type_i = at_pairs[i_pair][0]
-      type_j = at_pairs[i_pair][1]
-      print >> qeam_file, "<per_pair_data atomic_num_i=\"{2}\" atomic_num_j=\"{3}\" r_min=\"{0}\" r_cut=\"{1}\">".format(self.rmin, self.rcut, type_i, type_j)
-      print >> qeam_file, "<spline_V>"
-      phi = spline(r, V[:,i_pair], k=3)
-      for i, d in enumerate(r): 
-        if d>=self.rmin and i%self.ds==0:
-          print >> qeam_file, '\t\t\t<point r=\"{0}\" y=\" {2} \" b=\" {1} \" c=\" {1} \" d=\" {1} \"/>'.format(d, '0.0000000', 0.5*V[i,i_pair])
-      print >> qeam_file, "</spline_V>"
-      print >> qeam_file, "</per_pair_data>"
-
-    at_pairs = [[26,1],[1,26]]
+    pot_num = 0
+    for i_pair in range(n_spec):
+      for j_pair in range(i_pair, n_spec):
+        type_i = at_types[i_pair]
+        type_j = at_types[j_pair]
+        print at_types[i_pair], at_types[j_pair], pot_num
+        print >> qeam_file, "<per_pair_data atomic_num_i=\"{2}\" atomic_num_j=\"{3}\" r_min=\"{0}\" r_cut=\"{1}\">".format(self.rmin, self.rcut, type_i, type_j)
+        print >> qeam_file, "<spline_V>"
+        phi = spline(r, V[:,pot_num], k=3)
+        for i, d in enumerate(r): 
+          if d>=self.rmin and i%self.ds==0:
+            print >> qeam_file, '\t\t\t<point r=\"{0}\" y=\" {2} \" b=\" {1} \" c=\" {1} \" d=\" {1} \"/>'.format(d, '0.0000000', V[i,pot_num])
+        pot_num += 1
+        print >> qeam_file, "</spline_V>"
+        print >> qeam_file, "</per_pair_data>"
 #Now write PAIR Density Splines: 
-    #for i_pair in range(2):
-     # type_i = at_pairs[i_pair][0]
-     # type_j = at_pairs[i_pair][1]
-     # print >> qeam_file, "<per_pair_data atomic_num_i=\"{2}\" atomic_num_j=\"{3}\" r_min=\"{0}\" r_cut=\"{1}\">".format(self.rmin, self.rcut, type_i, type_j)
-     # print >> qeam_file, "<spline_rho>"
-     # for i, d in enumerate(zip(r, rho[:,2+i_pair])): 
-     #   if d[0] >= self.rmin and i%self.ds==0:
-     #     print >> qeam_file, '\t\t\t<point r=\"{0:.7f}\" y=\" {2:.7f} \" b=\" {1} \" c=\" {1} \" d=\" {1} \"/>'.format(d[0], '0.0000000', d[1])
-     # print >> qeam_file, "</spline_rho>"
-     # print >> qeam_file, "</per_pair_data>"
+#    if True: 
+#      for i_pair in range(1):
+#        type_i = at_pairs[i_pair][0]
+#        type_j = at_pairs[i_pair][1]
+#        print >> qeam_file, "<per_pair_data atomic_num_i=\"{2}\" atomic_num_j=\"{3}\" r_min=\"{0}\" r_cut=\"{1}\">".format(self.rmin, self.rcut, type_i, type_j)
+#        print >> qeam_file, "<spline_rho>"
+#        rhotmp = rho[:,2]
+#        for i, d in enumerate(zip(r, rhotmp)): 
+#          if d[0] >= self.rmin and i%self.ds==0:
+#            print >> qeam_file, '\t\t\t<point r=\"{0:.7f}\" y=\" {2:.7f} \" b=\" {1} \" c=\" {1} \" d=\" {1} \"/>'.format(d[0], '0.0000000',
+#            d[1]+rho[i,3])
+#        print >> qeam_file, "</spline_rho>"
+#        print >> qeam_file, "</per_pair_data>"
     print >> qeam_file, "</EAM_ErcolAd_params>"
 
 if __name__ == '__main__':
 #If we convert the file into the Fe.alloy format then ase can read it directly.
-  eam_parse = EAMXML(rmin=0.0, rcut=5.3, ds=10)
+  eam_parse = EAMXML(rmin=0.0, rcut=5.3, ds=5.0)
   PLOT_POT = False
   CONV_TO_XML = True
-  n_spec   = 2
+  n_spec   = 4
   n_pairs  = n_spec*(n_spec+1)/2
   print n_spec, n_pairs
   n_points = 10000
@@ -98,19 +100,28 @@ if __name__ == '__main__':
 #Atom Pair Potential
   V   = np.zeros([n_points, n_pairs])
 #Atom Pair Potential
-  rho = np.zeros([n_points, 4])
+  rho = np.zeros([n_points, n_spec])
 #Uncomment to read in Dudarev potential:
   if CONV_TO_XML:
-    F[:,0]   = map(float, open('./FeHPotB/Fe_F.dat','r').read().split())
-    V[:,0]   = map(float, open('./FeHPotB/Fe_Fe_V.dat','r').read().split())
-    rho[:,0] = map(float, open('./FeHPotB/Fe_Fe_rho.dat','r').read().split())
-    F[:,1]   = map(float, open('./FeHPotB/H_F.dat','r').read().split())
-    V[:,1]   = map(float, open('./FeHPotB/H_H_V.dat','r').read().split())
-    rho[:,1] = map(float, open('./FeHPotB/H_H_rho.dat','r').read().split())
-    V[:,2]   = map(float, open('./FeHPotB/Fe_H_V.dat','r').read().split())
-    rho[:,2] = map(float, open('./FeHPotB/Fe_H_rho.dat','r').read().split())
-    rho[:,3] = map(float, open('./FeHPotB/H_Fe_rho.dat','r').read().split())
-    eam_parse.alloy_to_xml(n_spec=2, F=F, V=V, rho=rho)
+    F[:,0]   = map(float, open('./Fe_F.dat','r').read().split())
+    F[:,1]   = map(float, open('./H_F.dat','r').read().split())
+# For additional fictional types so that we can include extra
+# density terms we just repeat the embedding and pair potentials
+# They are never accessed in the dynamics...
+    F[:,2]   = map(float, open('./H_F.dat','r').read().split())
+    F[:,3]   = map(float, open('./H_F.dat','r').read().split())
+    V[:,0]   = map(float, open('./Fe_Fe_V.dat','r').read().split())
+    V[:,1]   = map(float, open('./Fe_H_V.dat','r').read().split())
+    V[:,2]   = map(float, open('./H_H_V.dat','r').read().split())
+#Fill up the other pair potentials with 0
+    for i_num in range(3,10):
+      #V[:,i_num]   = map(float, open('./H_H_V.dat','r').read().split())
+      V[:,i_num]   = V[:,2]
+    rho[:,0] = map(float, open('./Fe_Fe_rho.dat','r').read().split())
+    rho[:,1] = map(float, open('./H_H_rho.dat','r').read().split())
+    rho[:,2] = map(float, open('./Fe_H_rho.dat','r').read().split())
+    rho[:,3] = map(float, open('./H_Fe_rho.dat','r').read().split())
+    eam_parse.alloy_to_xml(n_spec=n_spec, F=F, V=V, rho=rho)
 
   if PLOT_POT:
     with open('quip.xml', 'r') as f:
