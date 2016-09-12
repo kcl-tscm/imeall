3c63d0d9d51aaf3720f3ac80cb43d534e50fb048
Montmorency <lamberh@tcd.ie>
2016-09-12 16:51:05 +0100

    update to calc_phonon

--- imeall/calc_phonon.py
+++ imeall/calc_phonon.py
@@ -1,11 +1,11 @@
 import os
 import numpy as np
-from ase import Atoms as aseAtoms
+from ase.io   import vasp
+from ase      import Atoms as aseAtoms
 from quippy   import Atoms, Potential, Atoms
 from phonopy  import Phonopy
 from phonopy.structure.atoms import PhonopyAtoms
 from ase.lattice.cubic       import BodyCenteredCubic
-
 
 try:
   pot_dir  = os.environ['POTDIR']
@@ -16,18 +16,21 @@ except:
 pot_file = os.path.join(pot_dir, 'PotBH.xml')
 pot     = Potential('IP EAM_ErcolAd', param_filename=pot_file)
 
-unit_cell = BodyCenteredCubic(directions = [[1,0,0], [0,1,0],[0,0,1]],
-                              size = (1,1,1), symbol='Fe', pbc=(1,1,1),
-                              latticeconstant = 2.83)
+#unit_cell = BodyCenteredCubic(directions = [[1,0,0], [0,1,0],[0,0,1]],
+#                              size = (1,1,1), symbol='Fe', pbc=(1,1,1),
+#                              latticeconstant = 2.83)
+
+unit_cell = vasp.read_vasp('SPOSCAR')
+
 symbols          = unit_cell.get_chemical_symbols()
 cell             = unit_cell.get_cell()
 scaled_positions = unit_cell.get_scaled_positions()
 
 
-n_sup_cell = 8
+n_sup_cell = 1
 THZ_to_mev = 4.135665538536
 unit_cell  = PhonopyAtoms(symbols=symbols, cell=cell, scaled_positions=scaled_positions)
-phonon     = Phonopy(unit_cell, [[n_sup_cell,0,0],[0,n_sup_cell,0],[0,0,n_sup_cell]]) 
+phonon     = Phonopy(unit_cell, [[n_sup_cell,0,0],[0,n_sup_cell,0],[0,0,1]]) 
 phonon.generate_displacements(distance=0.1)
 supercells = phonon.get_supercells_with_displacements()
 #We need to get the forces on these atoms...
@@ -40,11 +43,11 @@ for sc in supercells:
   forces.append(cell.get_forces())
 phonon.set_forces(forces)
 print phonon.produce_force_constants()
-mesh = [20, 20, 20]
+mesh = [16, 16, 1]
 phonon.set_mesh(mesh, is_eigenvectors=True)
 qpoints, weights, frequencies, eigvecs = phonon.get_mesh()
 #SHOW DOS STRUCTURE
-phonon.set_total_DOS(freq_min=0.0, freq_max=60.0)
+phonon.set_total_DOS(freq_min=0.0, freq_max=30.0)
 phonon.get_total_DOS()
 phonon.write_total_DOS()
 phonon.plot_total_DOS().show()
