1adbaf2b9b0a3bc2c184696221bdc5a8216d4624
Montmorency <henry.lambert@kcl.ac.uk>
2016-09-14 14:33:49 +0000

    local changes to imeall to run on mira

--- imeall/__init__.py
+++ imeall/__init__.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask, render_template
+try:
+  from flask import Flask, render_template
+except:
+  print 'No Flask Server Available'
 
-app             = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('config.py')
-
-from imeall import views
+if __name__=='__main__':
+  app             = Flask(__name__, instance_relative_config=True)
+  app.config.from_pyfile('config.py')
+  from imeall import views
