5d64369016e607e42d74a6738aa58e5cd3c457f7
Montmorency <lamberh@tcd.ie>
2017-04-28 12:37:22 +0100

    atoms.copy bug in calc_elast_dipole, also fortran indexing for remove atoms. added gen_vasp to make vasp incars.

--- imeall/calc_elast_dipole.py
+++ imeall/calc_elast_dipole.py
@@ -1,15 +1,13 @@
 import os
 import argparse
 import subprocess
+import numpy as np
 from ase.constraints import UnitCellFilter, StrainFilter
-from quippy import Atoms, set_fortran_indexing
+from quippy import Atoms, set_fortran_indexing, Potential
 
 set_fortran_indexing(False)
 
 class ElasticDipole(object):
-  def __init__(self,):
-    self.strains=[−0.01, −0.005, 0, 0.005, 0.01]
-
   def calc_interatomic_force(self, defect_index, at_index):
     """
     Should be in QUIP.
@@ -37,55 +35,72 @@ class ElasticDipole(object):
       :relax_cell: relax lattice parameters
     """
     from ase.optimize import FIRE
-    strain_mask  = [1,1,1,0,0,0]
     if relax_cell:
+      strain_mask  = [1,1,1,0,0,0]
       ucf = UnitCellFilter(ats, strain_mask)
       opt = FIRE(ucf)
     else:
       opt = FIRE(ats)
     opt.run(fmax = force_tol)
-    ats.write(name)
+    ats.write(output_name)
     return ats
   
-  def compute_vacancy_dipole(defect, pot, input_cell='defect_cell_relaxed.xyz'):
-    ats = Atoms(input_cell)
+  def compute_vacancy_dipole(self, defect, ats, pot):
+    """
+    :method: Valid where there is a clear distinction between host lattice
+    and the defect atom.
+    """
+    ats.remove_atoms(defect.index+1)
     ats.set_calculator(pot)
-    del(ats[defect.index])
-    forces = ats.get_atomic_forces()
+    ats.write('relaxed_cell_removed_defect.xyz')
+    forces = ats.get_forces()
     #calculat dipole tensor
-    alpha_ij = np.zeros(3,3)
-    for at in ats:
-      for k in range(3):
-        if at.index != defect.index:
-          self.calc_interatomic_force(at, defect_atom)
-          alpha_ij[0, 0] = defect_force[0]*(at.position[0]-defect.position[0])
-          alpha_ij[0, 1] = defect_force[0]*(at.position[1]-defect.position[1])
-          alpha_ij[0, 2] = defect_force[0]*(at.position[2]-defect.position[2])
-          alpha_ij[1, 0] = defect_force[1]*(at.position[0]-defect.position[0])
-          alpha_ij[1, 1] = defect_force[1]*(at.position[1]-defect.position[1])
-          alpha_ij[1, 2] = defect_force[1]*(at.position[2]-defect.position[2])
-          alpha_ij[2, 0] = defect_force[2]*(at.position[0]-defect.position[0])
-          alpha_ij[2, 1] = defect_force[2]*(at.position[1]-defect.position[1])
-          alpha_ij[2, 2] = defect_force[2]*(at.position[2]-defect.position[2])
+    alpha_ij = np.zeros([3,3])
+    for defect_force, at in zip(forces, ats):
+      alpha_ij[0, 0] += defect_force[0]*(defect.position[0] - at.position[0])
+      alpha_ij[0, 1] += defect_force[0]*(defect.position[1] - at.position[1])
+      alpha_ij[0, 2] += defect_force[0]*(defect.position[2] - at.position[2])
+      alpha_ij[1, 0] += defect_force[1]*(defect.position[0] - at.position[0])
+      alpha_ij[1, 1] += defect_force[1]*(defect.position[1] - at.position[1])
+      alpha_ij[1, 2] += defect_force[1]*(defect.position[2] - at.position[2])
+      alpha_ij[2, 0] += defect_force[2]*(defect.position[0] - at.position[0])
+      alpha_ij[2, 1] += defect_force[2]*(defect.position[1] - at.position[1])
+      alpha_ij[2, 2] += defect_force[2]*(defect.position[2] - at.position[2])
     return alpha_ij
 
-if __name__='__main__':
+if __name__=='__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argumet('-i', '--input_file', help="Structure file of system with defect. (xyz)", required=True)
-  parser.add_argumet('-d', '--defect_index', help="Atom index (base 0) of the defect atom.", require=True)
-
+  parser.add_argument('-i', '--input_file', help="Structure file of system with defect. (xyz)", required=True)
+  parser.add_argument('-d', '--defect_index', help="Atom index (base 0) of the defect atom.", type=int)
+  parser.add_argument('-rc', '--relax_cell', help="Relax defect super cell.", action='store_true')
+  parser.add_argument('-f', '--force_tol', help="force_tolerance.", type=float, default=0.0001)
+  args = parser.parse_args()
   try:
     POT_DIR     = os.environ['POTDIR']
   except:
     sys.exit("PLEASE SET export POTDIR='path/to/potfiles/'")
+
   elastic = ElasticDipole()  
   eam_pot = os.path.join(POT_DIR, 'PotBH.xml')
   pot = Potential('IP EAM_ErcolAd do_rescale_r=T r_scale={0}'.format(1.00894848312), param_filename=eam_pot)
 
 # relax defect cell
   ats = Atoms(args.input_file)
-  defect_index = 0
-  defect = ats[defect_index]
-  elastic.relax_defect_cell(ats)
+  ats.set_calculator(pot)
+
+  init_vol = ats.get_volume()
+  print 'Initial Vol.', init_vol
+  elastic.relax_defect_cell(ats, force_tol=args.force_tol, relax_cell=args.relax_cell)
+  final_vol = ats.get_volume()
+  print 'Final Vol.', final_vol
+  print 'Diff.', final_vol-init_vol
 # compute dipole tensor
-  elastic.compute_vacancy_dipole(defect)
+  ats = Atoms('defect_cell_relaxed.xyz')
+  h_list = [at for at in ats if at.number==1]
+  if len(h_list) > 1:
+    sys.exit('too many hydrogens in unit cell')
+  else:
+    defect = h_list[0]
+  print 'Defect index', defect.index, 'Position', defect.position, 'Type: ', defect.number
+  ats.set_calculator(pot)
+  print elastic.compute_vacancy_dipole(defect, ats.copy(), pot)
