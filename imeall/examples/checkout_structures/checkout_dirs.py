5a15b7ed83bf7e639e8418859c48c4391e85eed4
Montmorency <lamberh@tcd.ie>
2017-09-11 17:35:34 +0100

    updated checkout_dirs example to use tilt or twist.

--- imeall/examples/checkout_structures/checkout_dirs.py
+++ imeall/examples/checkout_structures/checkout_dirs.py
@@ -1,5 +1,7 @@
 from imeall.models import GBQuery
 
 query = GBQuery()
-query.copy_gb_dirtree(material='alphaFe', or_axis='0,0,1', pots=['PotBH.xml'], target_dir='./checkout')
+#modify gb_type to tilt or twist to checkout the appropriate boundary type.
+query.copy_gb_dirtree(material='alphaFe', or_axis='0,0,1', 
+                      pots=['PotBH.xml'], target_dir='./checkout', gb_type='tilt')
 
