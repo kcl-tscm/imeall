5a84840812fe19b3ce933af2ca2f1cb6ed9665ad
Montmorency <lamberh@tcd.ie>
2017-09-12 13:51:02 +0100

    increasing navbar centrality and size

--- imeall/ovito_scripts/attach_cna.py
+++ imeall/ovito_scripts/attach_cna.py
@@ -11,5 +11,6 @@ node = import_file(args.input_file)
 cna = CommonNeighborAnalysisModifier()
 node.modifiers.append(cna)
 node.compute()
-export_file(node, "output.xyz", "xyz", columns = ["Particle Identifier", "Particle Type", "Position.X", "Position.Y", "Position.Z",
+export_file(node, "output.xyz", "xyz", columns = ["Particle Identifier", "Particle Type",
+																									"Position.X", "Position.Y", "Position.Z",
                                                   "Potential Energy", "Structure Type"])
