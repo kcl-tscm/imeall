5572d2359587f9ac6169caff985de9f83dc3c409
Montmorency <lamberh@tcd.ie>
2017-09-29 11:46:46 +0100

    interstitial test working

---
+++ imeall/examples/boundary_interstitials/clean.py
@@ -0,0 +1,16 @@
+import os
+
+files_to_remove = ['POSCAR',
+'h_energetics.xyz',
+'h_energetics.xyz.idx',
+'h_site_ener_stretch_0.0.txt',
+'hydrogenated_grain.xyz',
+'imeall.log',
+'interface.xyz',
+'interface.xyz.idx',
+'output.xyz.idx',
+'unique_h_sites.json',
+'unique_lattice_sites.json']
+
+for f in files_to_remove:
+  os.remove(f)
