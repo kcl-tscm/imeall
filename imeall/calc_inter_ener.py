91f3c1f28b756c41117bd6f3ac2174ba30ee5c00
Montmorency <lamberh@tcd.ie>
2017-09-29 15:26:43 +0100

    another POT_DIR found

--- imeall/calc_inter_ener.py
+++ imeall/calc_inter_ener.py
@@ -100,7 +100,7 @@ def apply_strain(ats, mode, st_num):
 alat = 2.83
 #bulk_sites = [tetrahedral site and octahedral]
 bulk_sites = map(lambda x: alat*x, [np.array([0.25, 0.0, 0.5]), np.array([0,0,0.5])])
-POT_DIR = os.environ['POTDIR']
+POT_DIR = os.path.join(app.root_path, 'potentials')
 eam_pot = os.path.join(POT_DIR, 'PotBH.xml')
 r_scale = 1.00894848312
 pot = Potential('IP EAM_ErcolAd do_rescale_r=T r_scale={0}'.format(r_scale), param_filename=eam_pot)
