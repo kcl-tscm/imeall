cafcc90e377f4d2e3a02eb8b6e724bb7da6b1d4a
montmorency <lamberh@tcd.ie>
2018-02-21 13:41:18 +0000

    stashing local NEB calculations.

--- imeall/lotf/calc_NEB_qmmm.py
+++ imeall/lotf/calc_NEB_qmmm.py
@@ -135,7 +135,7 @@ parser.add_argument("--qm_radius", "-q", type=float, default=2.0)
 parser.add_argument("--use_socket", "-u", action="store_false")
 parser.add_argument("--neb_path", "-n", nargs="+", type=float, default=[0.25, 0, -0.25])
 parser.add_argument("--fmax", "-f", type=float, help="maximum force for relaxation.", default=0.01)
-parser.add_argument("--sup_cell", "-s", type=int, help="size of fe matrix super cell")
+parser.add_argument("--sup_cell", "-s", type=int, help="size of fe matrix super cell", default=5)
 parser.add_argument("--knots", "-kn", type=int, help="number of images", default=11)
 parser.add_argument("--k", "-k", type=float, default=5.0, help="spring constant for NEB (default eV/A^2).")
 parser.add_argument("--input_file", "-i", default="fe_bcc_h.xyz", help="input file.")
@@ -173,7 +173,7 @@ print ("together with the buffer of %.1f" % (qm_radius + buff ) +
 magmoms=[2.6 for _ in range(np.count_nonzero(qm_buffer_mask))]
 vasp_args = dict(xc='PBE', amix=0.01, amin=0.001, bmix=0.001, amix_mag=0.01, bmix_mag=0.001,
                  kpts=[1, 1, 1], kpar=1, lreal='auto', nelmdl=-15, ispin=2, prec='Accurate', ediff=1.e-3,
-                 nelm=100, algo='VeryFast', lplane=False, lwave=False, lcharg=False, istart=0, encut=320,
+                 nelm=100, algo='VeryFast', lplane=False, lwave=False, lcharg=False, istart=0, encut=420,
                  magmom=magmoms, maxmix=30, #https://www.vasp.at/vasp-workshop/slides/handsonIV.pdf #for badly behaved clusters.
                  voskown=0, ismear=1, sigma=0.1, isym=0) # possibly try iwavpr=12, should be faster if it works
 
